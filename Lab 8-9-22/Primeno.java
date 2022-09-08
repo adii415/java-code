fb1c879a6dfdd80902cebcb9a5576a89fa89a47c
adii415 <113023893+adii415@users.noreply.github.com>
2022-09-08 18:04:41 +0530

    Add files via upload

---
+++ Lab 8-9-22/Primeno.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Primeno
+{ 
+           public static void main(String args[])
+           {
+                  Scanner s=new Scanner(System.in);
+
+                  System.out.println("Enter your number : ");
+                  int no=s.nextInt();
+                  int temp=0;
+                  for(int i=2;i<=no-1;i++)
+                  {
+                              if(no%i==0)
+                              {
+                                   temp=temp+1;
+                              }
+                   }
+                   if(temp==0)
+                   {
+                         System.out.println(no+"is prime no");
+                   }
+                   else
+                   {
+                         System.out.println(no+"is not prime");
+                   }
+           
+         }
+}
