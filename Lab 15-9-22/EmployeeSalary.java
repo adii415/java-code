8d94a1284876104fb19b26230ac43ee53a68e801
adii415 <113023893+adii415@users.noreply.github.com>
2022-09-15 18:54:22 +0530

    Add files via upload

---
+++ Lab 15-9-22/EmployeeSalary.java
@@ -0,0 +1,11 @@
+class EmployeeSalary
+{
+	public static void main (String ...args)
+	{
+		int average = 10000;
+		int emp1 = 11000;
+		int emp2 = 5000;
+		int emp3 = (average * 3) - (emp1 + emp2) ; 
+		System.out.println(" The average salary will be " + emp3);
+	}
+}
